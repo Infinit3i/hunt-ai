42af2f22e12529ed6019e06fd09c24474c4cb562
infinit3i <matthewiver@protonmail.com>
2025-02-14 19:16:55 -0500

    added the important sections

--- Modules/technique_template.py
+++ Modules/technique_template.py
@@ -1,7 +1,7 @@
 def get_content():
     return {
-        "id": "",
-        "url_id": "",
+        "id": "",           # T1556.001
+        "url_id": "",       # 1556/001
         "title": "",
         "tactic": "",
         "data_sources": "",
@@ -15,13 +15,13 @@ def get_content():
             {"type": "", "source": "", "destination": ""}
         ],
         "detection_methods": [],
-        "spl_query": [],
-        "hunt_steps": [],
+        "spl_query": [], # spl queries to detect the technique
+        "hunt_steps": [], # steps to hunt for the technique
         "expected_outcomes": [],
         "false_positive":"",
-        "clearing_steps": [],
+        "clearing_steps": [], # steps on machine to remove the technique
         "mitre_mapping": [
-            {"tactic": "", "technique": "", "example": ""}
+            {"tactic": "", "technique": "", "example": ""} # next technique that would happen after
         ],
         "watchlist": [],
         "enhancements": [],
