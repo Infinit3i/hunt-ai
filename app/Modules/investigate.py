ab3a41d03b304563b9036a6bfd773f419199f6f7
infinit3i <matthewiver@protonmail.com>
2025-02-20 00:10:15 -0500

    remove extra garb

--- app/Modules/investigate.py
+++ app/Modules/investigate.py
@@ -6,7 +6,6 @@ def get_investigate_content():
         "title": "Investigate",
         "description": "Explore and analyze potential threats using the resources and tools provided.",
         "resources": [
-            {"name": "IP", "url": "https://start.me/p/qbzw4e/cyber-security"},
             {"name": "IP", "url": "/investigate/ip"},
             {"name": "Domain", "url": "/investigate/domain"},
             {"name": "File Hash", "url": "/investigate/filehash"},
